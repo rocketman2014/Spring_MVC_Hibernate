24723084144e5edc368b55085e6118db6bc3775c
Алексей Алдокимов <141712991+rocketman2014@users.noreply.github.com>
2023-12-21 11:10:18 +0300

    Fix pom

--- src/main/java/ru/aldokimov/spring/controllers/UserController.java
+++ src/main/java/ru/aldokimov/spring/controllers/UserController.java
@@ -1,10 +1,8 @@
 package ru.aldokimov.spring.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
